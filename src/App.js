91574a7ee17c5d3d1c556524bb8843c437211364
GZMaster <33158650+GZMaster@users.noreply.github.com>
2023-01-22 23:30:18 +0100

    setting up app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
 import "./App.scss";
+import logo from "./assets/images/acesLogo_small.png";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="logo_div">
+        <img src={logo} alt="logo" />
+      </div>
+
+      <Outlet />
+    </div>
+  );
 }
 
 export default App;
